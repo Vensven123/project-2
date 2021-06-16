4bb999584379420a43ff655eedb4e5a3bd4e1081
Vensven123 <78368102+Vensven123@users.noreply.github.com>
2021-06-17 00:18:27 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,35 @@
-# project-2
+# project-2/model.py
+
+
+from django.db import models
+
+Department = (('Maths', 'maths'),
+              ('Science', 'Science'),
+              ('sports', 'sports')
+              )
+
+
+# Create your models here.
+
+class Subject(models.Model):
+    subject_Type = models.CharField(max_length=50, default='')
+
+    def __str__(self):
+        return self.subject_Type
+
+
+class Questions(models.Model):
+    subject = models.ForeignKey(Subject,on_delete=models.CASCADE)
+    #Subject = models.CharField(max_length=50,choices='Department')
+    Question = models.TextField(max_length=500, default='')
+    option_1 = models.CharField(max_length=100, default='')
+    option_2 = models.CharField(max_length=100, default='')
+    option_3 = models.CharField(max_length=100, default='')
+    option_4 = models.CharField(max_length=100, default='')
+    Answer = models.CharField(max_length=100, default='')
+    #Answer = models.IntegerField()
+
+    def __str__(self):
+        return self.Question
+
+
